34c2c5f31f8e7b13ad6138db9419e6c69f3837ad
bl3nddddd <woodoo2013@gmail.com>
2022-04-10 21:29:35 +0700

    add prettier, eslint and remote styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from 'app/app-routing.module';
 import { AppComponent } from 'app/app.component';
-import {AuthModule} from "app/auth/auth.module";
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthModule } from 'app/auth/auth.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AuthModule,
-    NgbModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, AuthModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
